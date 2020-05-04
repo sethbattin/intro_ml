afffd59cc62e37efca5d1f392a9ce94db916480d
Seth Battin <seth.battin@gmail.com>
2020-05-03 20:50:27 -0500

    add downloader

---
+++ datasets/agaricus-lepiota/get.sh
@@ -0,0 +1,3 @@
+wget  https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.names
+
