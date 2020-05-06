8167af09432088dc98ebd95692b176600c14fd9b
Seth Battin <seth.battin@gmail.com>
2020-05-06 00:52:49 -0500

    create component probabilities, plan final output

--- classifier.js
+++ classifier.js
@@ -16,7 +16,32 @@ const bayesDiscrete = (data, {classColumn, headingRow}) => {
     })
   });
 
-  console.log(attributeCounts, classCounts);
+  const probabilities = {};
+  Object.entries(attributeCounts).forEach(([classification, attributes]) => {
+    probabilities[classification] = classCounts[classification] / data.length;
+    Object.entries(attributes).forEach(([attribute, variations]) => {
+      Object.entries(variations).forEach(([variation, value]) => {
+        const attributeVariation = `${attribute}:${variation}`;
+        probabilities[`${attributeVariation}|${classification}`] = value / classCounts[classification];
+        probabilities[attributeVariation] = value / data.length + (probabilities[attributeVariation] || 0);
+      });
+    });
+  });
+
+  const instancePis = {};
+  data.forEach((instance, i) => {
+    Object.keys(classCounts).forEach((classification) => {
+      // bedtime pseudocode:
+      // print the probability expression for the instance with its known class
+      // print the individual attribute probabilities
+      // print the net probability (big-PI product of all), expect > inverse
+    })
+  });
+  
+
+  
+
+  console.log(attributeCounts, probabilities);
 
 };
 
