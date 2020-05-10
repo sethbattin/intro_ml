ecc00826b39abd3ea0e84034f710bd12dfa56396
Seth Battin <seth.battin@gmail.com>
2020-05-10 02:10:46 -0500

    allow processing varied data with --headingRow and --classColumn

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ https://link.springer.com/book/10.1007%2F978-3-319-63913-0
 The books contains a stale link to UC Irvine's Machine Learning Repository.  The current link is https://archive.ics.uci.edu/ml/index.php
 
 I will check in relevant descriptions of sets, but probably leave the data itself outside this repo.
+
+### Processing options
+--classColumn (default: 0) specifies which column of the data to treat that the classification
+--headingRow (default: first line of data) specifies headings to use for columns in the data
+
+pies example:
+```bash
+node csvReader.js < datasets/book-pies/pies.data --classColumn=5
+```
+
+mushrooms example:
+```bash
+node csvReader.js < datasets/agaricus-lepiota/agaricus-lepiota.data --headingRow="class,cap-shape,cap-surface,cap-color,bruises?,odor,gill-attachment,gill-spacing,gill-size,gill-color,stalk-shape,stalk-root,stalk-surface-above-ring,stalk-surface-below-ring,stalk-color-above-ring,stalk-color-below-ring,veil-type,veil-color,ring-number,ring-type,spore-print-color,population,habitat"
+```
--- csvReader.js
+++ csvReader.js
@@ -3,17 +3,41 @@ const classifier = require('./classifier');
 
 const rl = readline.createInterface({input: process.stdin});
 
+// should probably use a cli library here.  
+const classColumn = parseInt(
+  process.argv.includes('--classColumn') ?
+    process.argv[process.argv.indexOf('--classColumn') + 1] :
+    process.argv.reduce(
+      (arg, v) => v.match('--classColumn=') ? v.split('=')[1] : arg,
+      0
+    ),
+  10);
+
 const data = [];
+let headingRow = 
+  process.argv.includes('--headingRow') ?
+    process.argv[provess.argv.indexOf('--headingRow') + 1] :
+    process.argv.reduce(
+      (arg, v) => v.match('--headingRow=') ? v.split('=')[1].split(',') : arg,
+      null
+    )
+;
 
 rl.on('line', (line) => {
   data.push(line.split(','));
+  if (!headingRow) {
+    headingRow = data.pop();
+  }
 });
 rl.on('close', () => {
-  const headingRow = data.shift();
-  const trained = classifier.bayesDiscrete(data, {classColumn: 5, headingRow});
+  const trained = classifier.bayesDiscrete(data, {classColumn, headingRow});
 
-  const testInstance = ['Square', 'Thick', 'Gray', 'Thin', 'White'];
+  let testInstance = [...data.pop()]
+  testInstance.splice(classColumn, 1);
   const classification = trained.classify(testInstance, {debug: true});
 
-  console.log(testInstance, classification);
+//  console.log(testInstance, classification,
+//    headingRow
+  //  trained.probabilities
+//  );
 });
